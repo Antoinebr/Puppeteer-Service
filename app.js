a8aea163add665e07160b2fc0e8a69c1f0c3e416
Antoine Brossault <antoinebrossault@gmail.com>
2019-04-02 12:58:17 +0100

    mobile screenshot route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const thirdpartyCtrl = require('./controllers/thirdpartyCtrl');
 const coverageCtrl = require('./controllers/coverageCtrl');
-
+const screenshotCtrl = require('./controllers/screenshotCtrl')
 
 const app = express();
 app.use(cors());
@@ -10,6 +10,7 @@ app.use(cors());
 // router 
 app.get('/get3p', thirdpartyCtrl.get3p);
 app.get('/coverage',coverageCtrl.sendCoverage);
+app.get('/screenshot',screenshotCtrl.takeScreenshot);
 
 
 module.exports = app;
---
+++ controllers/screenshotCtrl.js
@@ -0,0 +1,83 @@
+const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
+const phone = devices['Nexus 5X'];
+
+exports.takeScreenshot = async (request,response) => {
+
+  const url = request.query.url;
+  if (!url) {
+    return response.status(400).send(
+      'Please provide a URL. Example: ?url=https://example.com');
+  }
+
+  // Default to a reasonably large viewport for full page screenshots.
+  const viewport = {
+    width: 1280,
+    height: 1024,
+    deviceScaleFactor: 2
+  };
+
+  let fullPage = true;
+  const size = request.query.size;
+  if (size) {
+    const [width, height] = size.split(',').map(item => Number(item));
+    if (!(isFinite(width) && isFinite(height))) {
+      return response.status(400).send(
+        'Malformed size parameter. Example: ?size=800,600');
+    }
+    viewport.width = width;
+    viewport.height = height;
+
+    fullPage = false;
+  }
+
+  const browser = await puppeteer.launch({
+    dumpio: true,
+    // headless: false,
+    // executablePath: 'google-chrome',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'], // , '--disable-dev-shm-usage']
+  });
+
+  try {
+    const page = await browser.newPage();
+    await page.setViewport(viewport);
+    await page.emulate(phone);
+    await page.goto(url, {
+      waitUntil: 'networkidle0'
+    });
+
+    const opts = {
+      fullPage,
+      // omitBackground: true
+    };
+
+    if (!fullPage) {
+      opts.clip = {
+        x: 0,
+        y: 0,
+        width: viewport.width,
+        height: viewport.height
+      };
+    }
+
+    let buffer;
+
+    const element = request.query.element;
+    if (element) {
+      const elementHandle = await page.$(element);
+      if (!elementHandle) {
+        return response.status(404).send(
+          `Element ${element} not found`);
+      }
+      buffer = await elementHandle.screenshot();
+    } else {
+      buffer = await page.screenshot(opts);
+    }
+    response.type('image/png').send(buffer);
+  } catch (err) {
+    response.status(500).send(err.toString());
+  }
+
+  await browser.close();
+
+}
